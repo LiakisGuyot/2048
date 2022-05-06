550e1f0846e0b68f516e2b61b97562e3f4c07f66
Benjamin Guyot de La Pommeraye <benjamin.guyot-de-la-pommeraye@etu.univ-lyon1.fr>
2022-05-06 16:38:10 +0200

    j ai n oté d es tru cs

--- src/modele/Case.java
+++ src/modele/Case.java
@@ -3,13 +3,19 @@ package modele;
 public class Case {
     private int valeur;
 
+    private boolean fusionnable;
+
     public Case(int _valeur) {
         valeur = _valeur;
+        fusionnable = true;
     }
 
     public int getValeur() {
         return valeur;
     }
+    public void setValeur(int valeur) {this.valeur = valeur;}
+    public boolean isFusionable() {return fusionnable;}
+    public void setFusionnable(boolean fusionnable) {this.fusionnable = fusionnable;}
 
     public void Fuse() {
         valeur *= 2;
